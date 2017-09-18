f376c2b593d67722e0a44ee17e26145a53298c57
Adrian Fahrer <acfahrer@gmail.com>
2017-09-18 01:58:00 -0400

    upload

---
+++ assets/js/papp.js
@@ -0,0 +1,40 @@
+
+function p1() {
+    myWindow = window.open("assets/apps/Psychic-Game/index.html", "myWindow", "width=600,height=400");
+
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}    
+
+function p2() {
+    myWindow = window.open("assets/apps/Gem-Collector/index.html", "myWindow", "width=600,height=768");
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}
+
+function p3() {
+    myWindow = window.open("assets/apps/TriviaGame/index.html", "myWindow", "width=800,height=800");
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}
+
+function p4() {
+    myWindow = window.open("assets/apps/Train-Scheduler/index.html", "myWindow", "width=800,height=900");
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}
+
+function p5() {
+    myWindow = window.open("assets/apps/SmashIT/groupproject4.html", "myWindow", "width=600,height=400");
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}
+
+function p6() {
+    myWindow = window.open("https://agile-ridge-33089.herokuapp.com/", "myWindow", "width=600,height=500");
+    myWindow.moveTo(500, 100);                                  // Moves the new window    
+    myWindow.focus();                                           // Sets focus to the new window
+}
+
+
+2122
